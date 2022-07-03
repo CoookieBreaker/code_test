89238171992445cec7309399e5cc02bb5e7a686c
CoookieBreaker <dun05066@gmail.com>
2022-07-03 23:30:43 +0900

    commit

---
+++ Programmers/0011.java
@@ -0,0 +1,12 @@
+//Programmers
+//문자열을 정수로 바꾸기
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
