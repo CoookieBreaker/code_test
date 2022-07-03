dc83ae4f0d490d7898b903b2357efd6f2b045ebf
CoookieBreaker <dun05066@gmail.com>
2022-07-03 23:45:58 +0900

    commit

---
+++ Programmers/0012.java
@@ -0,0 +1,14 @@
+//Programmers
+//x만큼 간격이 있는 n개의 숫자
+
+class Solution {
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        
+        for(int i = 0; i < n; i++){
+            answer[i] = x * (i+1);
+        }
+        
+        return answer;
+    }
+}
