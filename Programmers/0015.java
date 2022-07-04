7f8c1fa7f14da4e154d56eb5c93dd54523e72ce2
CoookieBreaker <dun05066@gmail.com>
2022-07-04 23:59:02 +0900

    commit

--- Programmers/0015.java
+++ Programmers/0015.java
@@ -1,3 +1,18 @@
 //Programmers
 //문자열 다루기 기본
 
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        
+        if(s.length() == 4 || s.length() == 6){
+             for(int i = 0; i < s.length(); i++){
+                 if (!Character.isDigit(s.charAt(i))) {
+                    answer = false;
+                 }
+             }
+        }
+        
+        return answer;
+    }
+}
