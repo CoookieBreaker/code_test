598ae61aec1453f6f7055d822c96f6fb200cfa2f
CoookieBreaker <dun05066@gmail.com>
2022-07-09 00:41:10 +0900

    commit

--- Programmers/0016.java
+++ Programmers/0016.java
@@ -1,2 +1,24 @@
 //Programmers
-//문자열 다루기 기본
+//하샤드 수
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+
+        String num = String.valueOf(x);
+        int result = 0;
+
+        for(int i = 0; i < num.length(); i++){
+            String tmp = num.substring(i, i+1);
+            result += Integer.parseInt(tmp);
+        }
+        
+        if(x % result != 0){
+            answer = false;
+        }
+        
+        
+        
+        return answer;
+    }
+}
