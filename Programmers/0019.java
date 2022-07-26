2373187505fff203b9d093e804c055da0aa0c4a5
Yebin Lim <dun05066@gmail.com>
2022-07-26 20:01:15 +0900

    commit

--- Programmers/0019.java
+++ Programmers/0019.java
@@ -1,3 +1,18 @@
 //Programmers
-//
-ASDF
+//없는 숫자 더하기
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for(int i = 0; i < 10; i++){
+            for(int n : numbers){
+                if(n == i) answer += n;
+            }
+        }
+        
+        answer -= 45;
+        
+        
+        return -answer;
+    }
+}
