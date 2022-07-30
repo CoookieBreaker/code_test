900572a9121c37e2c04f1b0d68d6e6d701b51955
CoookieBreaker <dun05066@gmail.com>
2022-07-30 23:39:26 +0900

    commit

---
+++ Programmers/0028.java
@@ -0,0 +1,12 @@
+//Programmers
+//두 정수 사이의 합
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        for(int i = (a < b ? a : b); i <= (a < b ? b : a); i++) answer += i;
+        
+        
+        return answer;
+    }
+}
